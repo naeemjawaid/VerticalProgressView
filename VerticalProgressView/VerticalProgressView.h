4fc99db2d638aa2c165d480e97d4df4792d73275
Muhammad Naeem Jawaid <naeemjawaid@gmail.com>
2017-07-23 17:03:07 +0500

    initial commit

---
+++ VerticalProgressView/VerticalProgressView.h
@@ -0,0 +1,19 @@
+//
+//  VerticalProgressView.h
+//  VerticalProgressView
+//
+//  Created by Muhammad Naeem Jawaid on 23/07/2017.
+//  Copyright © 2017 Muhammad Naeem Jawaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VerticalProgressView.
+FOUNDATION_EXPORT double VerticalProgressViewVersionNumber;
+
+//! Project version string for VerticalProgressView.
+FOUNDATION_EXPORT const unsigned char VerticalProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerticalProgressView/PublicHeader.h>
+
+
---
+++ VerticalProgressView/VerticalProgressView.swift
@@ -0,0 +1,131 @@
+//
+//  VerticalSliderView.swift
+//  MovieApp
+//
+//  Created by Muhammad Naeem Jawaid on 23/07/2017.
+//  Copyright © 2017 Muhammad Naeem Jawaid. All rights reserved.
+//
+
+import UIKit
+
+open class VerticalProgressView: UIView {
+    
+    let progressView: UIProgressView = {
+        let progressView = UIProgressView()
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        return progressView
+    }()
+    
+    open var progress: Float {
+        get {
+            return progressView.progress
+        }
+        set {
+            progressView.progress = newValue
+        }
+    }
+    
+    open var progressViewStyle: UIProgressViewStyle {
+        get {
+            return progressView.progressViewStyle
+        }
+        set {
+            progressView.progressViewStyle = newValue
+        }
+    }
+    
+    open var progressTintColor: UIColor? {
+        get {
+            return progressView.progressTintColor
+        }
+        set {
+            progressView.progressTintColor = newValue
+        }
+    }
+    
+    open var progressImage: UIImage? {
+        get {
+            return progressView.progressImage
+        }
+        set {
+            progressView.progressImage = newValue
+        }
+    }
+    
+    open var trackTintColor: UIColor? {
+        get {
+            return progressView.trackTintColor
+        }
+        set {
+            progressView.trackTintColor = newValue
+        }
+    }
+    
+    open var trackImage: UIImage? {
+        get {
+            return progressView.trackImage
+        }
+        set {
+            progressView.trackImage = newValue
+        }
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        initialize()
+    }
+    
+    required override public init(frame: CGRect) {
+        super.init(frame: frame)
+        progressView.frame = frame
+        initialize()
+    }
+    
+    convenience init(progressViewStyle style: UIProgressViewStyle) {
+        self.init()
+        initialize()
+        progressView.progressViewStyle = style
+    }
+    
+    fileprivate func initialize() {
+        rotateSlider()
+        addSubview(progressView)
+    }
+    
+    fileprivate func rotateSlider() {
+        if !ascending {
+            progressView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi) * -0.5)
+        } else {
+            progressView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi) * 0.5)
+        }
+    }
+    
+    open var ascending: Bool = false {
+        didSet {
+            rotateSlider()
+        }
+    }
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let width = bounds.width
+        let height = bounds.height
+        
+        progressView.bounds.size.width = height
+        progressView.bounds.size.height = width
+        progressView.center.x = bounds.midX
+        progressView.center.y = bounds.midY
+    }
+    
+    override open var intrinsicContentSize: CGSize {
+        get {
+            return CGSize(width: progressView.intrinsicContentSize.height, height: progressView.intrinsicContentSize.width)
+        }
+    }
+    
+    open func setProgress(_ progress: Float, animated: Bool) {
+        progressView.setProgress(progress, animated: animated)
+    }
+}
